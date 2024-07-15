3adcb56eca586bdc01950d8d908f39ca06db99b9
hayeonbae7 <139304636+hayeonbae7@users.noreply.github.com>
2024-07-15 17:06:57 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -5,8 +5,7 @@ def solution(num_list):
         while num != 1:
             if num % 2 == 0:
                 num = num / 2
-                answer += 1
             else:
                 num = (num -1)/2
-                answer += 1
+            answer += 1
     return answer

5abf8b5bfe1ecfdcb7c35b6b343ac0ffd4a261f3
hayeonbae7 <139304636+hayeonbae7@users.noreply.github.com>
2024-07-18 23:55:02 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,9 @@
 function solution(array, n) {
-    var answer = 0;
-    for (let i = 0; i <= array.length; i++){
-        if (array[i] === n) answer += 1
-    }
+    let answer = 0;
+    // for (let i = 0; i <= array.length; i++){
+    //     if (array[i] === n) answer += 1
+    // }
+    const Array = array.filter((item) => item === n)
+    answer = Array.length
     return answer;
 }

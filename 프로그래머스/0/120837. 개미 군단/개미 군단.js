27b15178a1e6e96b85fc487925a7fc8207d3c998
hayeonbae7 <139304636+hayeonbae7@users.noreply.github.com>
2024-07-21 15:07:13 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,9 +1,9 @@
 function solution(hp) {
     var answer = 0;
     hp5 = hp % 5
-    answer += parseInt(hp / 5)
+    answer += ~~(hp / 5)
     hp3 = hp5 % 3
-    answer += parseInt(hp5 /3)
+    answer += ~~(hp5 /3)
     answer += hp3
     
     return answer;

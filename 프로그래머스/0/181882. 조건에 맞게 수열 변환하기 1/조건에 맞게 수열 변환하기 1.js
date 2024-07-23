95e8554a0aeb1f3f53af5836daf71a2357c254b6
hayeonbae7 <139304636+hayeonbae7@users.noreply.github.com>
2024-07-23 20:59:22 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 63.63 ms, Memory: 137 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    var answer = [];
+    arr.map((item) => {
+        if(item >= 50 && item % 2 == 0){
+            answer.push(item/2)
+        } else if(item < 50 && item % 2 !== 0){
+            answer.push(item*2)
+        } else answer.push(item)
+    })
+    return answer;
+}
